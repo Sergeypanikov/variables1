737b5a8a2753d1e9d22c93bc046c365de85f6a81
Sergeypanikov <svp1967@gmail.com>
2024-12-03 11:37:21 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+number_of_homework_assignments_completed = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = 1.5 / 12
+print("course_name:" + "Python")
+print("number_of_homework_assignments_completed:")
+print(number_of_homework_assignments_completed)
+print("number_of_hours_spent:")
+print(number_of_hours_spent)
+print("time_for_one_task:")
+print(time_for_one_task)
+print("Курс:",course_name,", Всего_задач:",number_of_homework_assignments_completed, ", Затрачено часов:",number_of_hours_spent,", Среднее время выполнения:",time_for_one_task,"часа.")
